f5364986608566945b3f89c683d68a2b8e90e028
Anthony Fielding <ant@antfie.com>
2022-11-17 22:07:38 +0000

    add -trimpath to the build flags

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 VERSION="v1.3"
 FLAGS="-X main.AppVersion=$VERSION -s -w"
 
-env GOOS=darwin GOARCH=arm64 go build -ldflags="$FLAGS" -o "dist/scan_compare-mac-arm64" .
-env GOOS=darwin GOARCH=amd64 go build -ldflags="$FLAGS" -o "dist/scan_compare-mac-amd64" .
-env GOOS=linux GOARCH=amd64 go build -ldflags="$FLAGS" -o "dist/scan_compare-linux-amd64" .
-env GOOS=windows GOARCH=amd64 go build -ldflags="$FLAGS" -o "dist/scan_compare-win.exe" .
+env GOOS=darwin GOARCH=arm64 go build -ldflags="$FLAGS" -trimpath -o "dist/scan_compare-mac-arm64" .
+env GOOS=darwin GOARCH=amd64 go build -ldflags="$FLAGS" -trimpath -o "dist/scan_compare-mac-amd64" .
+env GOOS=linux GOARCH=amd64 go build -ldflags="$FLAGS" -trimpath -o "dist/scan_compare-linux-amd64" .
+env GOOS=windows GOARCH=amd64 go build -ldflags="$FLAGS" -trimpath -o "dist/scan_compare-win.exe" .
