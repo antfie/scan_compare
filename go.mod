263f27133cb27cf4d335ee67afe0bab6c38aad94
Anthony Fielding <ant@antfie.com>
2024-02-22 09:56:44 +0000

    Update dependencies

--- go.mod
+++ go.mod
@@ -13,5 +13,5 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 )
---
+++ scripts/test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Exit if any command fails
+set -e
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[1;36m'
+NC='\033[0m' # No Color
+
+echo -e "${CYAN}Linting...${NC}"
+gofmt -s -w .
+
+echo -e "${CYAN}Running go vet...${NC}"
+go vet ./...
+
+echo -e "${CYAN}Running gosec...${NC}"
+set +e # Don't fail on error for this
+gosec ./...
+set -1
+
+echo -e "${CYAN}Testing...${NC}"
+
+go test -v -coverprofile coverage.out -failfast -shuffle on ./...
+go tool cover -html=coverage.out -o coverage.html
